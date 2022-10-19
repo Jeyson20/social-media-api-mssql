55ae7d3a6beedb35306302f749dde7252ab0c409
HUMANO\jjalmonte <jjalmonte@humano.com.do>
2022-10-19 16:46:14 -0400

    adding  posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { PostsModule } from './module/posts/posts.module';
 import { UsersModule } from './module/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    UsersModule]
+    UsersModule,
+    PostsModule]
 })
-export class AppModule { 
+export class AppModule {
   // static port: number;
 
   // constructor(private readonly configService: ConfigService) {
