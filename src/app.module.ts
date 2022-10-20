13bbad0075a2368ec832c3a0999690336708cefd
Jeyson Almonte <jeysom@live.com>
2022-10-20 02:44:22 -0400

    rename module to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-import { PostsModule } from './module/posts/posts.module';
-import { UsersModule } from './module/users/users.module';
-import { CommentService } from './module/comment/comment.service';
-import { CommentController } from './module/comment/comment.controller';
-import { CommentModule } from './module/comment/comment.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -13,10 +10,8 @@ import { CommentModule } from './module/comment/comment.module';
     DatabaseModule,
     UsersModule,
     PostsModule,
-    CommentModule,
   ],
-  providers: [CommentService],
-  controllers: [CommentController],
+
 })
 export class AppModule {
   static port: number;
