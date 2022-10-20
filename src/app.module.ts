a122de7ef5036a0b8c6ebef6b944dae6d4d0186b
Jeyson Almonte <jeysom@live.com>
2022-10-20 02:50:02 -0400

    resolving conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-<<<<<<< HEAD
 import { PostsModule } from './modules/posts/posts.module';
 import { UsersModule } from './modules/users/users.module';
-=======
-import { PostsModule } from './module/posts/posts.module';
-import { UsersModule } from './module/users/users.module';
-import { CommentService } from './module/comment/comment.service';
-import { CommentController } from './module/comment/comment.controller';
-import { CommentModule } from './module/comment/comment.module';
->>>>>>> cfdacaf7ee9d4eb84e2167940269c9ce50a3a3de
 
 @Module({
   imports: [
@@ -18,15 +10,8 @@ import { CommentModule } from './module/comment/comment.module';
     DatabaseModule,
     UsersModule,
     PostsModule,
-<<<<<<< HEAD
   ],
 
-=======
-    CommentModule,
-  ],
-  providers: [CommentService],
-  controllers: [CommentController],
->>>>>>> cfdacaf7ee9d4eb84e2167940269c9ce50a3a3de
 })
 export class AppModule {
   static port: number;
