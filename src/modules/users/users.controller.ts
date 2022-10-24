0238dfde5c64f9f01cce7819dc9d65e9d2e2dc82
HUMANO\jjalmonte <jjalmonte@humano.com.do>
2022-10-24 01:04:26 -0400

    [ADD] users services and controllers

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) { }
+
+    @Get()
+    getUsers(): Promise<any> {
+        return this.usersService.getUsers()
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: number): Promise<any> {
+        return this.usersService.getUserById(id);
+    }
+
+    @Get('/userByEmail/:email')
+    getUserByEmail(@Param("email") email: string): Promise<any> {
+        return this.usersService.getUserByEmail(email);
+    }
+
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { MssqlService } from 'src/database/services';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private readonly mssqlService: MssqlService) { }
+
+    async getUsers(): Promise<any> {
+
+        try {
+
+            const conn = await this.mssqlService.getConnection();
+            const result = (await conn.request().execute('SP_GET_USERS')).recordsets;
+
+            const data = {
+                response: result[1][0],
+                data: result[0],
+            }
+
+            return data;
+
+        } catch (ex) {
+            throw new BadRequestException(ex.message)
+        }
+    }
+
+    async getUserById(id: Number): Promise<any> {
+
+        try {
+
+            const conn = await this.mssqlService.getConnection();
+            const result = (await conn.request()
+                .input('UserId', id)
+                .execute('SP_GET_USER')).recordsets;
+
+            const data = {
+                response: result[1][0],
+                data: result[0],
+            }
+
+            return data;
+
+        } catch (ex) {
+            throw new BadRequestException(ex.message)
+        }
+    }
+
+    async getUserByEmail(email: string): Promise<any> {
+
+        try {
+
+            const conn = await this.mssqlService.getConnection();
+            const result = (await conn.request()
+                .input('email', email)
+                .execute('SP_GET_USER_BY_EMAIL')).recordsets;
+                
+            const data = {
+                response: result[1][0],
+                data: result[0],
+            }
+
+            return data;
+
+        } catch (ex) {
+            throw new BadRequestException(ex.message)
+        }
+    }
+}
