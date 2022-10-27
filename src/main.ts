3c460e6c258cd3b799e518f6358e7cdf6cc698b7
HUMANO\jjalmonte <jjalmonte@humano.com.do>
2022-10-27 01:51:25 -0400

    [ADD] global validation models

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
+
+  //Usar validacion de modelos global
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(AppModule.port || 3000);
 }
 bootstrap().then(() => {
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,14 +3,13 @@ import { AuthService } from './auth.service';
 import { BodySigninUserDto, BodySignupUserDto } from './dto';
 
 @Controller('auth')
-@UsePipes(new ValidationPipe({ skipMissingProperties: true }))
-@UsePipes(new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true, transform: true }))
+// @UsePipes(new ValidationPipe({ skipMissingProperties: true }))
+// @UsePipes(new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true, transform: true }))
 
 export class AuthController {
     constructor(readonly authService: AuthService) { }
 
     @Post('/signup')
-
     async signup(@Body() params: BodySignupUserDto) {
         const data = await this.authService.signup(params);
         return data;
