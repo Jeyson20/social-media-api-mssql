8677da7e202e8bb1b7fef01095086d9922009856
Jeyson Almonte <jjalmonte@humano.com.do>
2023-03-08 00:50:21 -0400

    [REF] colunm-utils

--- src/common/utils/column-utils.ts
+++ src/common/utils/column-utils.ts
@@ -1,16 +1,22 @@
-
 export const renameObjectKeys = (obj: any): any => {
+    
     if (obj === null) return null;
     if (typeof obj !== "object") return obj;
 
     if (Array.isArray(obj)) {
         return obj.map((elemento: any) => renameObjectKeys(elemento));
-      }
+    }
 
     const result = {};
+
     for (const [key, value] of Object.entries(obj)) {
+
         const camelCaseKey = key.charAt(0).toLowerCase() + key.slice(1);
-        result[camelCaseKey] = value !== null ? renameObjectKeys(value) : null;
+
+        if (value instanceof Date) 
+            result[camelCaseKey] = value.toISOString()
+        else
+            result[camelCaseKey] = value !== null ? renameObjectKeys(value) : null;
     }
     return result;
 }
